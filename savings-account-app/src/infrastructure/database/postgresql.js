35b2dd78db06d716893b84d4cced844bb9b6df92
Victor <victor.98.javier@gmail.com>
2024-08-13 16:30:10 +0200

    Use env and set db and service in the same network

--- savings-account-app/src/infrastructure/database/postgresql.js
+++ savings-account-app/src/infrastructure/database/postgresql.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'litethinking',
-    password: 'postgres',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = {
